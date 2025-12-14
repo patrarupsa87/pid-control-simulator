59c8cf5a8c78cf7b8e977a22dac238b7a9163eee
patrarupsa87 <patrarupsa87@gmail.com>
2025-12-14 21:15:56 +0530

    PID added

--- pidctrl.c
+++ pidctrl.c
@@ -10,19 +10,32 @@ int main(){
     double kp=0.001;
     double ki;
     double integral=0;
+    double integral_max=10;
+    double integral_min=-10;
+    double kd=0.001;
+    double derivative;
+    double previous_error=0;
     tau=0.3;
     setpoint =1.0;
     dt=0.5;
     output=0.0;
     ki=0.2;
     for(time=0; time<=10.0; time=time+dt){
+        if (integral>integral_max){
+            integral=integral_max;
+        }
+        if(integral<integral_min){
+            integral=integral_min;
+        }
         error=setpoint-output;
+        derivative=(error-previous_error)/dt;
         integral=integral+error*dt;
-        control=kp*error+ki*integral;
+        control=kp*error+ki*integral+kd*derivative;
         output=output+(control-output)*dt/tau;
         printf("%lf  | ",time);
         printf("%lf  |",output);
         printf("%lf \n",control);
+        previous_error=error;
     }
     
 }
